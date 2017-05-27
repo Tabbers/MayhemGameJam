f4ca081142d3532fc3656b607b73a64826bd653c
moritzfink <if14b011@technikum-wien.ta>
2017-05-27 15:03:48 +0200

    Dog Game throw mechanics added

---
+++ MayhemGameJam2017/Assets/dog_game/ArrowMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMovement : MonoBehaviour 
+{
+	[HideInInspector]
+	public float force;
+	public float speed = 2;
+	public GameObject stickPrefab;
+
+	private bool stickShot = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+		diff.Normalize();
+
+		float rot_z = Mathf.Clamp(Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg, -30, 70);
+		transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
+
+		if (Input.GetMouseButtonDown (0) && !stickShot)
+		{
+			GameObject stickInstance = Instantiate (stickPrefab, transform.position, transform.rotation);
+			stickInstance.GetComponent<Stick> ().throwStick (force);
+			stickShot = true;
+		}
+	}
+}
---
+++ MayhemGameJam2017/Assets/dog_game/Stick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stick : MonoBehaviour 
+{
+	void Start()
+	{
+	}
+
+	public void throwStick(float velocity)
+	{
+		Vector3 rotatedNormalizedVector = gameObject.transform.rotation * Vector2.right;
+		rotatedNormalizedVector.Normalize ();
+		Vector2 forceVector = rotatedNormalizedVector * (velocity * 800);
+		gameObject.GetComponent<Rigidbody2D>().AddForce(forceVector);
+	}
+}
