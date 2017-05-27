06bdec0cc76c10dfcdf1270ad65041923abac801
Josua Kucher <gs15m008@technikum-wien.at>
2017-05-27 21:52:08 +0200

    - added ChoclateRain

---
+++ MayhemGameJam2017/Assets/Umbrella.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Umbrella : MonoBehaviour
+{
+
+    void Update()
+    {
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Clamp(Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg, 15, 170);
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
+    }
+}
---
+++ MayhemGameJam2017/Assets/umbrella_house.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class umbrella_house : MonoBehaviour {
+
+    GameObject player;
+    SceneController controller;
+    // Use this for initialization
+    void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("MainCharacter");
+        controller = GameObject.FindGameObjectWithTag("Controller").GetComponent<SceneController>();
+    }
+	void OnTriggerEnter2D()
+    {
+        Debug.Log("Enterd");
+        controller.LoadNextLevel();
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
