417c4520fe4abbc2a09a76196a34bf2b9c253aab
Josua Kucher <gs15m008@technikum-wien.at>
2017-05-27 10:43:32 +0200

    - moved CharacterController  - added basic Controller Outline

---
+++ MayhemGameJam2017/Assets/main_character/main_character_controller.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main_character_controller : MonoBehaviour
+{
+    public enum EMinigameState : int
+    {
+        EFootball = 0,
+        EUmbrella = 1,
+        EBallon,
+        EStickThrow
+    };
+
+    public EMinigameState minigameState;
+
+    void SetMinigameState(EMinigameState minigameState)
+    {
+        this.minigameState = minigameState;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        minigameState = EMinigameState.EFootball;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		switch(minigameState)
+        {
+            case EMinigameState.EFootball:
+                break;
+            case EMinigameState.EBallon:
+                break;
+            case EMinigameState.EStickThrow:
+                break;
+            case EMinigameState.EUmbrella:
+                Umbrella_Main();                
+                break;
+            default:
+                break;
+
+        }
+	}
+    private void Umbrella_Main()
+    {
+
+    }
+}
