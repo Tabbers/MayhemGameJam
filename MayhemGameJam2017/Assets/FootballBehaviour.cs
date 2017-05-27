52e71a5b4798cbf092285210e4a1d1024bece77f
Marcus <gs15m014@technikum-wien.at>
2017-05-27 14:13:54 +0200

    Added better football behaviour

--- MayhemGameJam2017/Assets/FootballBehaviour.cs
+++ MayhemGameJam2017/Assets/FootballBehaviour.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FootballBehaviour : MonoBehaviour {
 
     float traveledX;
+    float traveledY;
     Vector3 m_centerPosition;
     float m_degrees;
 
@@ -19,35 +20,74 @@ public class FootballBehaviour : MonoBehaviour {
     [SerializeField]
     float m_period = 1.0f;
 
+    bool bPlayBall = true;
+
+    int counter = 0;
     // Use this for initialization
-    void Start () {
+    void Start() {
         m_centerPosition = transform.position;
         traveledX = 0.0f;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
 
         float deltaTime = Time.deltaTime;
 
-        // Move center along x axis
-        float movementX = deltaTime * m_speed;
-        m_centerPosition.x += m_direction * movementX;
-
-        // Update degrees
-        float degreesPerSecond = 360.0f / m_period;
-        m_degrees = Mathf.Repeat(m_degrees + (deltaTime * degreesPerSecond), 360.0f);
-        float radians = m_degrees * Mathf.Deg2Rad;
-
-        // Offset by sin wave
-        Vector3 offset = new Vector3(0.0f, m_amplitude * Mathf.Sin(radians), 0.0f);
-        transform.position = m_centerPosition + offset;
-
-        traveledX += movementX;
-        if (traveledX > 15.0f)
+        if (bPlayBall)
         {
-            m_direction *= -1;
-            traveledX = 0.0f;
+            // Move center along x axis
+            float movementX = deltaTime * m_speed;
+            m_centerPosition.x += m_direction * movementX;
+
+            // Update degrees
+            float degreesPerSecond = 360.0f / m_period;
+            m_degrees = Mathf.Repeat(m_degrees + (deltaTime * degreesPerSecond), 360.0f);
+            float radians = m_degrees * Mathf.Deg2Rad;
+
+            // Offset by sin wave
+            Vector3 offset = new Vector3(0.0f, m_amplitude * Mathf.Sin(radians), 0.0f);
+            transform.position = m_centerPosition + offset;
+
+            counter++;            
         }
+        else
+        {
+            float movementY = deltaTime * m_speed;
+            m_centerPosition.y += m_direction * movementY;
+
+            // Update degrees
+            float degreesPerSecond = 360.0f / m_period;
+            m_degrees = Mathf.Repeat(m_degrees + (deltaTime * degreesPerSecond), 360.0f);
+            float radians = m_degrees * Mathf.Deg2Rad;
+
+            // Offset by sin wave
+            Vector3 offset = new Vector3(m_amplitude * Mathf.Sin(radians), 0.0f, 0.0f);
+            transform.position = m_centerPosition + offset;
+
+            traveledY += movementY;
+            if (traveledY > 6.0f)
+            {
+                if (m_direction > 0)
+                {
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    bPlayBall = true;
+                }                
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Football_Child1"))
+        {
+            m_direction = 1.0f;
+        }
+        else if (other.CompareTag("Football_Child2"))
+        {
+            m_direction = -1.0f;
+        }        
     }
 }
